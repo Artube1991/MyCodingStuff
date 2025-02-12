35fb37b6d5b2b7e9cf348be5d38a02e823b7f99c
Artube1991 <artube1991@gmail.com>
2025-02-12 15:28:04 +0200

    started new task

---
+++ castles-roads/castles-roads.sql
@@ -0,0 +1,27 @@
+CREATE TABLE castles(
+    castle_id INT AUTO_INCREMENT PRIMARY KEY,
+    castle_name VARCHAR(100)
+);
+
+INSERT INTO castles(castle_name)
+VALUES(
+    "Hogwarts"
+),
+(
+    "Stonehenge"
+),
+(
+    "Alnwick"
+),
+(
+    "Dover"
+),
+(
+    "The Tower Of London"
+),
+(
+    "Château Comtal de Carcassonne"
+),
+(
+    "Ceausescu Palace"
+);
