557fb384e1f1aefd0a76f2e6d3afb8529b2c8c88
Artube1991 <artube1991@gmail.com>
2025-07-12 15:46:21 +0300

    started to work with JS

---
+++ KeysTheGame/script.js
@@ -0,0 +1,9 @@
+let startButton = document.getElementById("start");
+
+const startTheGame = () => {
+    console.log("The button is clicked!");
+};
+
+document.addEventListener("click", startTheGame);
+
+console.log(startButton);
